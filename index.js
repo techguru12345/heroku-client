5942c6eaf86d19d7287732cb1db9357cbfa1941a
techguru12345 <112623110+techguru12345@users.noreply.github.com>
2022-09-07 15:13:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
- const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql2");
 
-const path = require('path')
+const db = mysql.createPool({
+host: " sql6.freemysqlhosting.net",
+user: "sql6517152",
+password: "fTQ3AjdfLi",
+database: "sql6517152",
+});
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
 
- const app = express()
+app.get("/api/get", (req, res)=>{
+    const sqlSelect =
+    "SELECT * FROM contactus";
+   db.query(sqlSelect, (err, result)=>{
+       res.send(result);
+    });
+})
+app.post("/api/insert",(req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const phone = req.body.phone
+    const subject = req.body.subject
+    const textarea = req.body.textarea
 
- app.use(express.static(path.join(__dirname + "/public")))
-
- const PORT = process.env.PORT || 5000
-
- app.listen(PORT)
+    const sqlInsert =
+     "INSERT INTO contactus (name, email, phone, subject, textarea) VALUES (?,?,?,?,?)"
+    db.query(sqlInsert, [name, email, phone, subject, textarea], (err, result)=>{
+        console.log(result);
+    });
+})
+app.listen(3306, () => {
+    console.log("server is runnig on port 3306");
+})
